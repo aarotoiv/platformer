ca99d9bd0da83c2cf8045dc8c1478284d2b4397b
aarotoiv <aaro.toivonen@gmail.com>
2020-01-28 03:23:37 +0200

    decent jumping

--- src/player.rs
+++ src/player.rs
@@ -7,6 +7,8 @@ use crate::world::World;
 pub struct Player {
     pub x_pos: f64,
     pub y_pos: f64,
+    x_vel: f64,
+    y_vel: f64,
     prev_x: f64,
     prev_y: f64,
     pub render_x: f64,
@@ -18,8 +20,6 @@ pub struct Player {
     pub direction: f64,
     falling: bool,
     touches: Touches,
-    jumping: bool,
-    jump_height: f64,
     wants_to_jump: bool
 }
 
@@ -40,6 +40,8 @@ impl Player {
         Player {
             x_pos: 0.0,
             y_pos: 0.0,
+            x_vel: 0.0,
+            y_vel: 0.0,
             prev_x: 0.0,
             prev_y: 0.0,
             render_x: *window_width as f64 / 2.0,
@@ -51,34 +53,31 @@ impl Player {
             direction: 1.0,
             falling: true,
             touches: Touches {left: false, right: false, top: false, bottom: false},
-            jumping: false,
-            jump_height: 0.0,
             wants_to_jump: false
         }
     }
 
     pub fn update(&mut self, args: &UpdateArgs) {
-        const X_ACCEL: f64 = 400.0;
-        const Y_ACCEL: f64 = 400.0;
-        const MAX_JUMP: f64 = -200.0;
+        const X_ACCEL: f64 = 500.0;
+        const Y_ACCEL: f64 = 50.0;
+        const JUMP_START: f64 = -2000.0;
 
         self.prev_x = self.x_pos;
         self.prev_y = self.y_pos;
 
-        self.falling = !self.touches.bottom && !self.jumping;
-
-        if self.touches.bottom {
-            self.jump_height = 0.0;
-        }
+        self.falling = !self.touches.bottom;
 
         let mut x_vel = 0.0;
-        let mut y_vel = 0.0;
 
         if self.keys.left {
             x_vel += -X_ACCEL;
         }
         if self.keys.right {
             x_vel += X_ACCEL;
+        }
+
+        if !self.keys.right && !self.keys.left {
+            
         }
         
         if x_vel > 0.0 {
@@ -88,24 +87,15 @@ impl Player {
         }
 
         if self.falling {
-            y_vel += Y_ACCEL;
+            self.y_vel += Y_ACCEL;
         }
-
-        if self.wants_to_jump && !self.jumping && !self.falling {
-            self.jumping = true;
+        println!("{}", self.falling);
+        if self.wants_to_jump && !self.falling {
+            self.y_vel = JUMP_START;
         }
-
-        if self.jumping && self.jump_height > MAX_JUMP {
-            y_vel += MAX_JUMP * 2.0;
-            self.jump_height += MAX_JUMP * 2.0 * args.dt;
-        } 
-
-        if self.jumping && self.jump_height < MAX_JUMP {
-            self.jumping = false;
-        }
-
+        //println!("{}", y_vel);
         self.x_pos += x_vel * args.dt;
-        self.y_pos += y_vel * args.dt;
+        self.y_pos += self.y_vel * args.dt;
 
     }
 
