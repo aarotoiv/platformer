1ead94d393ffe6b171952d713f1fb055cc9e6004
aarotoiv <aaro.toivonen@gmail.com>
2020-01-28 02:31:44 +0200

    bottom collisions working

--- src/player.rs
+++ src/player.rs
@@ -7,6 +7,8 @@ use crate::world::World;
 pub struct Player {
     pub x_pos: f64,
     pub y_pos: f64,
+    prev_x: f64,
+    prev_y: f64,
     pub render_x: f64,
     pub render_y: f64,
     pub scale: f64,
@@ -35,6 +37,8 @@ impl Player {
         Player {
             x_pos: 0.0,
             y_pos: 0.0,
+            prev_x: 0.0,
+            prev_y: 0.0,
             render_x: *window_width as f64 / 2.0,
             render_y: *window_height as f64 / 2.0,
             outline_scale: 0.05,
@@ -48,6 +52,11 @@ impl Player {
     }
 
     pub fn update(&mut self, args: &UpdateArgs) {
+        self.prev_x = self.x_pos;
+        self.prev_y = self.y_pos;
+
+        self.falling = !self.touches.bottom;
+
         const X_ACCEL: f64 = 400.0;
         const Y_ACCEL: f64 = 400.0;
         let mut x_vel = 0.0;
@@ -77,14 +86,27 @@ impl Player {
     }
 
     pub fn handle_collisions(&mut self, world: &World) {
+        let mut bottom = false;
+        //let mut top = false;
+        //let mut left = false; 
+        //let mut right = false;
+
         for block in world.blocks.iter() {
             let b_width = block.end_x - block.start_x;
             let b_height = block.end_y - block.start_y;
             let b_x = (block.start_x + block.end_x) / 2.0;
             let b_y = (block.start_y + block.end_y) / 2.0;
 
-
+            if self.x_pos - self.scale / 2.0 < b_x + b_width / 2.0 && self.x_pos + self.scale / 2.0 > b_x - b_width / 2.0
+            && self.y_pos + self.scale / 2.0 < b_y + b_height / 2.0 && self.y_pos + self.scale / 2.0 > b_y - b_height / 2.0 
+            && self.prev_y < b_y - b_height / 2.0 {   
+                bottom = true;
+                self.y_pos -= (self.y_pos + self.scale / 2.0) - (b_y - b_height / 2.0);  
+            }
+            
         }
+
+        self.touches.bottom = bottom;
     }
 
     pub fn handle_press(&mut self, key: Key) {
